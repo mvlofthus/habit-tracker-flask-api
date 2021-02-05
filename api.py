44a7208b1a1d85b015db6b7a2fcdc1c4a5e4c5aa
Mackenzie <mvlofthus@gmail.com>
2021-02-04 16:34:33 -0800

    built tables with relationships that pulled through

--- api.py
+++ api.py
@@ -25,14 +25,14 @@ ma = Marshmallow(app)
 
 
 class User(db.Model):
-    __tablename__ = 'users'
+    __tablename__ = 'user'
     
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(24))
     email = db.Column(db.String(64), nullable=False, unique=True)
     password = db.Column(db.String(64))
-    # tasks = db.relationship('Task', backref='user', lazy=True)
-    # goals = db.relationship('Goal', backref='user', lazy=True)
+    tasks = db.relationship('Task', backref='user', lazy=True)
+    goals = db.relationship('Goal', backref='user', lazy=True)
 
     def __init__(self, name, email, password):
         self.name = name
@@ -54,97 +54,97 @@ class UserSchema(ma.Schema):
 user_schema = UserSchema()
 users_schema = UserSchema(many=True)
 
-# class Task(db.Model):
-#     __tablename__ = 'tasks'
+class Task(db.Model):
+    __tablename__ = 'task'
     
-#     id = db.Column(db.Integer, primary_key = True)
-#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-#     category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
-#     goal_id = db.Column(db.Integer, db.ForeignKey('goal.id'), nullable=True)
-#     date = db.Column(db.Date)
-#     body = db.Column(db.Text)
+    id = db.Column(db.Integer, primary_key = True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
+    goal_id = db.Column(db.Integer, db.ForeignKey('goal.id'))
+    date = db.Column(db.Date)
+    body = db.Column(db.Text)
 
-#     def __init__(self, user_id, category_id, goal_id, date, body):
-#         self.user_id = user_id
-#         self.category_id = category_id
-#         self.goal_id = goal_id
-#         self.date = date
-#         self.body = body
+    def __init__(self, user_id, category_id, goal_id, date, body):
+        self.user_id = user_id
+        self.category_id = category_id
+        self.goal_id = goal_id
+        self.date = date
+        self.body = body
 
-#     def __repr__(self):
-#         return '<Task %r>' % self.task_id
+    def __repr__(self):
+        return '<Task %r>' % self.task_id
 
-# class TaskSchema(ma.Schema):
-#     class Meta:
-#             fields = (
-#                 'id',
-#                 'user_id',
-#                 'category_id',
-#                 'goal_id',
-#                 'date',
-#                 'body'
-#             )
+class TaskSchema(ma.Schema):
+    class Meta:
+            fields = (
+                'id',
+                'user_id',
+                'category_id',
+                'goal_id',
+                'date',
+                'body'
+            )
 
-# task_schema = TaskSchema()
-# tasks_schema = TaskSchema(many=True)
+task_schema = TaskSchema()
+tasks_schema = TaskSchema(many=True)
 
-# class Category(db.Model):
-#     __tablename__ = 'categories'
+class Category(db.Model):
+    __tablename__ = 'category'
     
-#     id = db.Column(db.Integer, primary_key = True)
-#     title = db.Column(db.String(24))
-#     tasks = db.relationship('Task', backref='category', lazy=True)
-#     goals = db.relationship('Goal', backref='category', lazy=True)
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(24))
+    tasks = db.relationship('Task', backref='category', lazy=True)
+    goals = db.relationship('Goal', backref='category', lazy=True)
 
-#     def __init__(self, title):
-#         self.title = title
+    def __init__(self, title):
+        self.title = title
 
-#     def __repr__(self):
-#         return '<Category %r>' % self.title
+    def __repr__(self):
+        return '<Category %r>' % self.title
 
-# class CategorySchema(ma.Schema):
-#     class Meta:
-#             fields = (
-#                 'id',
-#                 'title',
-#             )
+class CategorySchema(ma.Schema):
+    class Meta:
+            fields = (
+                'id',
+                'title',
+            )
 
-# category_schema = CategorySchema()
-# categories_schema = CategorySchema(many=True)
+category_schema = CategorySchema()
+categories_schema = CategorySchema(many=True)
 
-# class Goal(db.Model):
-#     __tablename__ = 'goals'
+class Goal(db.Model):
+    __tablename__ = 'goal'
     
-#     id = db.Column(db.Integer, primary_key = True)
-#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-#     category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=True)
-#     tag = db.Column(db.String(24))
-#     description = db.Column(db.String(64))
-#     weekly_freq = db.Column(db.Integer)
+    id = db.Column(db.Integer, primary_key = True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=True)
+    tag = db.Column(db.String(24))
+    description = db.Column(db.String(64))
+    weekly_freq = db.Column(db.Integer)
     
 
-#     def __init__(self, user_id, category_id, tag, description, weekly_freq):
-#         self.user_id = user_id
-#         self.category_id = category_id
-#         self.tag = tag
-#         self.description = description
-#         self.weekly_freq = weekly_freq
+    def __init__(self, user_id, category_id, tag, description, weekly_freq):
+        self.user_id = user_id
+        self.category_id = category_id
+        self.tag = tag
+        self.description = description
+        self.weekly_freq = weekly_freq
 
-#     def __repr__(self):
-#         return '<Goal %r>' % self.tag
+    def __repr__(self):
+        return '<Goal %r>' % self.tag
 
-# class GoalSchema(ma.Schema):
-#     class Meta:
-#         fields = (
-#             'user_id',
-#             'category_id',
-#             'tag',
-#             'description',
-#             'weekly_freq'
-#         )
+class GoalSchema(ma.Schema):
+    class Meta:
+        fields = (
+            'user_id',
+            'category_id',
+            'tag',
+            'description',
+            'weekly_freq'
+        )
 
-# goal_schema = GoalSchema()
-# goals_schema = GoalSchema(many=True)
+goal_schema = GoalSchema()
+goals_schema = GoalSchema(many=True)
 
 # db.drop_all()
 # db.create_all()
@@ -184,6 +184,20 @@ def user():
             } for user in users]
 
         return {"count": len(results), "users": results}
+    
+    # elif request.method == 'PUT':
+    #     data = request.get_json()
+    #     car.name = data['name']
+    #     car.model = data['model']
+    #     car.doors = data['doors']
+    #     db.session.add(car)
+    #     db.session.commit()
+    #     return {"message": f"car {car.name} successfully updated"}
+
+    # elif request.method == 'DELETE':
+    #     db.session.delete(car)
+    #     db.session.commit()
+    #     return {"message": f"Car {car.name} successfully deleted."}
 
 @app.route('/time')
 def get_current_time():
